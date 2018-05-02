d4e159d2af1c40bb7691e26f4fbdca1835e7b06d
Andrew Wallace <andrewallanwallace@gmail.com>
2018-05-02 22:04:36 +0800

    Site updated at 2018-05-02 14:04:36 UTC

--- javascripts/rec_dialog.js
+++ javascripts/rec_dialog.js
@@ -16,21 +16,22 @@ toastr.options = {
   "hideMethod": "fadeOut"
 };
 
-// /*var recDialogContent = '';
-// /*if (isMobile()) {
-//     recDialogContent = recDialogContent + '<p align="center">来自技术小黑屋的红包</p><a href="http://droidyue.com/2018_hongbaoyu.html"><img width="200" src="http://7jpolu.com1.z0.glb.clouddn.com/dahongbao.png"></a>';
-// } else {*/
-//     recDialogContent = recDialogContent + '<p align="center">微信赞赏支持持续原创</p><img width="200" src="https://asset.droidyue.com/image/zanshang_normal_qr.png">'
-// /*}*/
 
-// /*
-// <h3>知乎Live推荐</h3>
-// <ul>
-//   <li><a href="https://www.zhihu.com/lives/802899577341620224">我学安卓的那些套路</a></li>
-//   <li><a href="https://www.zhihu.com/lives/796775894273363968">程序员如何写好一篇技术文章？</a></li>
-// </ul>
-// */
+var recDialogContent = '';
+//if (isMobile()) {
+//    recDialogContent = recDialogContent + '<p align="center">来自技术小黑屋的红包</p><a href="http://droidyue.com/2018_hongbaoyu.html"><img width="200" src="http://7jpolu.com1.z0.glb.clouddn.com/dahongbao.png"></a>';
+//} else {*/
+recDialogContent = recDialogContent + '<p align="center">夯实Java基础课程推荐</p><img width="200" src="https://asset.droidyue.com/image/java_basic.jpeg">'
+/*}*/
 
-// if (true) {
-//     //toastr.info(recDialogContent, " ");
-// }
+/*
+<h3>知乎Live推荐</h3>
+<ul>
+  <li><a href="https://www.zhihu.com/lives/802899577341620224">我学安卓的那些套路</a></li>
+  <li><a href="https://www.zhihu.com/lives/796775894273363968">程序员如何写好一篇技术文章？</a></li>
+</ul>
+*/
+
+if (true) {
+    toastr.info(recDialogContent, " ");
+}
